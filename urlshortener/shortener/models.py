2dff75ed5423d7f2f9807d44842663334e9db781
Israel Cruz <israel.crz@outlook.com>
2021-04-02 14:49:32 -0600

    Model and database setup

--- urlshortener/shortener/models.py
+++ urlshortener/shortener/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Url(models.Model):
+    link = models.CharField(max_length=5000)
+    uuid = models.CharField(max_length=10)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Url'
+        verbose_name_plural = 'Urls'
